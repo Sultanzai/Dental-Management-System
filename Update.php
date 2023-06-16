1fd476e196796b9472b3a7a3685c65d28e51f14b
Emran SUltanzai <emransoltanzoy@gmail.com>
2023-06-16 15:50:56 +0430

    "fixed icons some designs"

--- Update.php
+++ Update.php
@@ -127,7 +127,7 @@
         $res = $con->query($sql);
 
         //Update paitent balance
-        $newsql = "UPDATE `tbl_patient_balance` SET `PB_Total`='$total',`PB_Receive`='$recevid',`PB_ReceiveDate`='$currentDate',`U_ID`='1' WHERE `P_ID`='$id'";
+        $newsql = "UPDATE `tbl_patient_balance` SET `PB_Total`='$total',`PB_Receive`='$recevid',`PB_ReceiveDate`='$currentDate',`U_ID`='$USERID' WHERE `P_ID`='$id'";
         $res2 = $con->query($newsql);
 
         if(!$res){
@@ -198,25 +198,25 @@ if($TYPE =="Admin"){
   <ul class='sidebar-list'>
     <li class=sidebar-list-item active id ='Dashboard' >
       <a href=Dashboard.php>
-        <svg xmlns=http://www.w3.org/2000/svg width=18 height=18 viewBox=0 0 24 24 fill=none stroke=currentColor stroke-width= 2  stroke-linecap= round  stroke-linejoin= round  class= feather feather-home ><path d= M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z /><polyline points= 9 22 9 12 15 12 15 22/></svg>
+        <svg xmlns=http://www.w3.org/2000/svg width='18' height='18' viewBox='0 0 24 24' fill=none stroke=currentColor stroke-width= 2  stroke-linecap= round  stroke-linejoin= round  class= feather feather-home ><path d= 'M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z' /><polyline points= '9 22 9 12 15 12 15 22/'></svg>
         <span>Dashboard</span>
       </a>
     </li>
     <li class= sidebar-list-item >
       <a href= index.php >
-        <svg xmlns= http://www.w3.org/2000/svg  width= 18  height= 18  viewBox= 0 0 24 24  fill= none  stroke= currentColor  stroke-width= 2  stroke-linecap= round  stroke-linejoin= round  class= feather feather-shopping-bag ><path d= M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z /><line x1= 3  y1= 6  x2= 21  y2= 6 /><path d= M16 10a4 4 0 0 1-8 0 /></svg>
+        <svg xmlns= 'http'://www.w3.org/2000/svg'  width= '18'  height= '18'  viewBox= '0 0 24 24'  fill= none  stroke= currentColor  stroke-width= 2  stroke-linecap= round  stroke-linejoin= round  class= feather feather-shopping-bag ><path d= 'M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z' /><line x1= '3'  y1= '6'  x2= '21'  y2= '6' /><path d= 'M16 10a4 4 0 0 1-8 0' /></svg>
         <span>Patients </span>
       </a>
     </li>
     <li class= sidebar-list-item >
       <a href= Report.php >
-        <svg xmlns= http://www.w3.org/2000/svg  width= 18  height= 18  viewBox= 0 0 24 24  fill= none  stroke= currentColor  stroke-width= 2  stroke-linecap= round  stroke-linejoin= round  class= feather feather-pie-chart ><path d= M21.21 15.89A10 10 0 1 1 8 2.83 /><path d= M22 12A10 10 0 0 0 12 2v10z /></svg>
+        <svg xmlns= http://www.w3.org/2000/svg  width= '18'  height= '18'  viewBox= '0 0 24 24'  fill= none  stroke= currentColor  stroke-width= 2  stroke-linecap= round  stroke-linejoin= round  class= feather feather-pie-chart ><path d= 'M21.21 15.89A10 10 0 1 1 8 2.83' /><path d= 'M22 12A10 10 0 0 0 12 2v10z' /></svg>
         <span>Report</span>
       </a>
     </li>
     <li class= sidebar-list-item >
       <a href= # >
-        <svg xmlns= http://www.w3.org/2000/svg  width= 18  height= 18  viewBox= 0 0 24 24  fill= none  stroke= currentColor  stroke-width= 2  stroke-linecap= round  stroke-linejoin= round  class= feather feather-inbox ><polyline points= 22 12 16 12 14 15 10 15 8 12 2 12 /><path d= M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z /></svg>
+        <svg xmlns= http://www.w3.org/2000/svg  width= '18'  height= '18'  viewBox= '0 0 24 24'  fill= none  stroke= currentColor  stroke-width= 2  stroke-linecap= round  stroke-linejoin= round  class= feather feather-inbox ><polyline points= '22 12 16 12 14 15 10 15 8 12 2 12' /><path d= 'M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z' /></svg>
         <span>Print Receipt</span>
       </a>
     </li>
@@ -230,13 +230,13 @@ else{
   <ul class='sidebar-list'>
     <li class= sidebar-list-item >
       <a href= index.php >
-        <svg xmlns= http://www.w3.org/2000/svg  width= 18  height= 18  viewBox= 0 0 24 24  fill= none  stroke= currentColor  stroke-width= 2  stroke-linecap= round  stroke-linejoin= round  class= feather feather-shopping-bag ><path d= M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z /><line x1= 3  y1= 6  x2= 21  y2= 6 /><path d= M16 10a4 4 0 0 1-8 0 /></svg>
+        <svg xmlns= http://www.w3.org/2000/svg  width= '18'  height= '18'  viewBox= '0 0 24 24'  fill= none  stroke= currentColor  stroke-width= 2  stroke-linecap= round  stroke-linejoin= round  class= feather feather-shopping-bag ><path d= M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z /><line x1= 3  y1= 6  x2= 21  y2= 6 /><path d= M16 10a4 4 0 0 1-8 0 /></svg>
         <span>Patients </span>
       </a>
     </li>
     <li class= sidebar-list-item >
       <a href= # >
-        <svg xmlns= http://www.w3.org/2000/svg  width= 18  height= 18  viewBox= 0 0 24 24  fill= none  stroke= currentColor  stroke-width= 2  stroke-linecap= round  stroke-linejoin= round  class= feather feather-inbox ><polyline points= 22 12 16 12 14 15 10 15 8 12 2 12 /><path d= M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z /></svg>
+        <svg xmlns= http://www.w3.org/2000/svg  width= '18'  height= '18'  viewBox= '0 0 24 24'  fill= none  stroke= currentColor  stroke-width= 2  stroke-linecap= round  stroke-linejoin= round  class= feather feather-inbox ><polyline points= 22 12 16 12 14 15 10 15 8 12 2 12 /><path d= M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z /></svg>
         <span>Print Receipt</span>
       </a>
     </li>
