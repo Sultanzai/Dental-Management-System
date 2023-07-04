682ef4208b6959777f1ecb3b7dc1e7263d1a1782
Emran SUltanzai <emransoltanzoy@gmail.com>
2023-07-04 11:11:46 +0430

    "Dash Board for expances Done"

---
+++ ExpanceRepot.php
@@ -0,0 +1,241 @@
+<?php
+    session_start(); 
+    $servername = "localhost";
+    $userName= "root";
+    $password = "";
+    $database = "dms";
+    // Create Connection
+    $con = new mysqli($servername, $userName, $password, $database);
+
+  
+    #User Data
+    $USERNAME = $_SESSION['Username'];
+    $USERID = $_SESSION['userid'];
+    $TYPE = $_SESSION['type'];
+
+
+    if(empty($USERID) OR empty($USERNAME) OR empty($TYPE)){
+      
+      header("location: /DMS/dist/login.php");
+      exit;
+    }
+    else{
+      $from = "";
+      $to = "";
+
+      if($_SERVER['REQUEST_METHOD']=="POST"){
+        $from = $_POST["from"];
+        $to = $_POST["to"];
+      }
+    }
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>DMS</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="NewStyle.css">
+
+  <!-- Boots strap-->
+  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="app-container">
+  <div class="sidebar">
+    <div class="sidebar-header">
+      <div class="app-icon">
+      <svg viewBox="0 0 512 512" xmlns="imgs/logo.PNG"><path fill="currentColor" /><img src ="imgs/logo.PNG"> </svg>
+      </div>
+    </div>
+
+    <!-- Left Links -->
+
+
+    <?php 
+
+if($TYPE =="Admin"){
+  echo"
+  <!-- Left Links -->
+  <ul class='sidebar-list'>
+    <li class=sidebar-list-item active id ='Dashboard' >
+      <a href=Dashboard.php>
+        <svg xmlns=http://www.w3.org/2000/svg width='18' height='18' viewBox='0 0 24 24' fill=none stroke=currentColor stroke-width= 2  stroke-linecap= round  stroke-linejoin= round  class= feather feather-home ><path d= 'M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z' /><polyline points= '9 22 9 12 15 12 15 22/'></svg>
+        <span>Dashboard</span>
+      </a>
+    </li>
+    <li class= sidebar-list-item >
+      <a href= index.php >
+        <svg xmlns= 'http'://www.w3.org/2000/svg'  width= '18'  height= '18'  viewBox= '0 0 24 24'  fill= none  stroke= currentColor  stroke-width= 2  stroke-linecap= round  stroke-linejoin= round  class= feather feather-shopping-bag ><path d= 'M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z' /><line x1= '3'  y1= '6'  x2= '21'  y2= '6' /><path d= 'M16 10a4 4 0 0 1-8 0' /></svg>
+        <span>Patients </span>
+      </a>
+    </li>
+    <li class= sidebar-list-item >
+      <a href= Report.php >
+        <svg xmlns= http://www.w3.org/2000/svg  width= '18'  height= '18'  viewBox= '0 0 24 24'  fill= none  stroke= currentColor  stroke-width= 2  stroke-linecap= round  stroke-linejoin= round  class= feather feather-pie-chart ><path d= 'M21.21 15.89A10 10 0 1 1 8 2.83' /><path d= 'M22 12A10 10 0 0 0 12 2v10z' /></svg>
+        <span>Report</span>
+      </a>
+    </li>
+    <li class= sidebar-list-item >
+      <a href= # >
+        <svg xmlns= http://www.w3.org/2000/svg  width= '18'  height= '18'  viewBox= '0 0 24 24'  fill= none  stroke= currentColor  stroke-width= 2  stroke-linecap= round  stroke-linejoin= round  class= feather feather-inbox ><polyline points= '22 12 16 12 14 15 10 15 8 12 2 12' /><path d= 'M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z' /></svg>
+        <span>Print Receipt</span>
+      </a>
+    </li>
+
+  </ul>
+  ";  
+}
+else{
+  echo"
+  <!-- Left Links -->
+  <ul class='sidebar-list'>
+    <li class= sidebar-list-item >
+      <a href= index.php >
+        <svg xmlns= http://www.w3.org/2000/svg  width= '18'  height= '18'  viewBox= '0 0 24 24'  fill= none  stroke= currentColor  stroke-width= 2  stroke-linecap= round  stroke-linejoin= round  class= feather feather-shopping-bag ><path d= M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z /><line x1= 3  y1= 6  x2= 21  y2= 6 /><path d= M16 10a4 4 0 0 1-8 0 /></svg>
+        <span>Patients </span>
+      </a>
+    </li>
+    <li class= sidebar-list-item >
+      <a href= # >
+        <svg xmlns= http://www.w3.org/2000/svg  width= '18'  height= '18'  viewBox= '0 0 24 24'  fill= none  stroke= currentColor  stroke-width= 2  stroke-linecap= round  stroke-linejoin= round  class= feather feather-inbox ><polyline points= 22 12 16 12 14 15 10 15 8 12 2 12 /><path d= M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z /></svg>
+        <span>Print Receipt</span>
+      </a>
+    </li>
+
+  </ul>
+  "; 
+}
+
+?>
+
+    <!--User Profile -->
+    <div class="account-info">
+
+    <div class="account-info-name"><?php echo"Welcome  <span>". $USERNAME; ?></div>
+      <button class="account-info-more">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"/><circle cx="19" cy="12" r="1"/><circle cx="5" cy="12" r="1"/></svg>
+      </button>
+    </div>
+
+
+  </div>
+  <div class="app-content">
+
+    <!-- Header Product with add product-->
+    <div class="app-content-header">
+      <h1 class="app-content-headerText">Report</h1>
+    </div>
+
+    <div class="container">
+    <form method="post" action="ExpanceRepot.php">
+      <div class="row">
+          <div class="col-md-8">
+            <div class="row" id="searchbar">
+              <div class="col-md-4"><span>From: </span> <input name="from" type="date"> </div>
+              <div class="col-md-4"><span> TO: </span> <input name="to" type="date"> </div>
+              <div class="col-md-4">
+                <a href='ExpanceRepot.php'>
+                  <button class='app-content-headerButton' type="submit" role="button"> Search </button>
+                </a>
+              </div>            
+            </div>
+          </div>
+        <div class="col-md-4"></div>
+    </form>  
+      </div>
+    </div>
+    
+
+    <div class="products-area-wrapper tableView">
+      <!-- Header Names -->
+      <section class="Patientlist">
+        <div class="products-header">
+          <div class="product-cell image">Expance ID <button class="sort-button">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512"><path fill="currentColor" d="M496.1 138.3L375.7 17.9c-7.9-7.9-20.6-7.9-28.5 0L226.9 138.3c-7.9 7.9-7.9 20.6 0 28.5 7.9 7.9 20.6 7.9 28.5 0l85.7-85.7v352.8c0 11.3 9.1 20.4 20.4 20.4 11.3 0 20.4-9.1 20.4-20.4V81.1l85.7 85.7c7.9 7.9 20.6 7.9 28.5 0 7.9-7.8 7.9-20.6 0-28.5zM287.1 347.2c-7.9-7.9-20.6-7.9-28.5 0l-85.7 85.7V80.1c0-11.3-9.1-20.4-20.4-20.4-11.3 0-20.4 9.1-20.4 20.4v352.8l-85.7-85.7c-7.9-7.9-20.6-7.9-28.5 0-7.9 7.9-7.9 20.6 0 28.5l120.4 120.4c7.9 7.9 20.6 7.9 28.5 0l120.4-120.4c7.8-7.9 7.8-20.7-.1-28.5z"/></svg>
+            </button>
+          </div>
+          <div class="product-cell category">Type<button class="sort-button">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512"><path fill="currentColor" d="M496.1 138.3L375.7 17.9c-7.9-7.9-20.6-7.9-28.5 0L226.9 138.3c-7.9 7.9-7.9 20.6 0 28.5 7.9 7.9 20.6 7.9 28.5 0l85.7-85.7v352.8c0 11.3 9.1 20.4 20.4 20.4 11.3 0 20.4-9.1 20.4-20.4V81.1l85.7 85.7c7.9 7.9 20.6 7.9 28.5 0 7.9-7.8 7.9-20.6 0-28.5zM287.1 347.2c-7.9-7.9-20.6-7.9-28.5 0l-85.7 85.7V80.1c0-11.3-9.1-20.4-20.4-20.4-11.3 0-20.4 9.1-20.4 20.4v352.8l-85.7-85.7c-7.9-7.9-20.6-7.9-28.5 0-7.9 7.9-7.9 20.6 0 28.5l120.4 120.4c7.9 7.9 20.6 7.9 28.5 0l120.4-120.4c7.8-7.9 7.8-20.7-.1-28.5z"/></svg>
+            </button>
+          </div>
+          <div class="product-cell category">Expance Info<button class="sort-button">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512"><path fill="currentColor" d="M496.1 138.3L375.7 17.9c-7.9-7.9-20.6-7.9-28.5 0L226.9 138.3c-7.9 7.9-7.9 20.6 0 28.5 7.9 7.9 20.6 7.9 28.5 0l85.7-85.7v352.8c0 11.3 9.1 20.4 20.4 20.4 11.3 0 20.4-9.1 20.4-20.4V81.1l85.7 85.7c7.9 7.9 20.6 7.9 28.5 0 7.9-7.8 7.9-20.6 0-28.5zM287.1 347.2c-7.9-7.9-20.6-7.9-28.5 0l-85.7 85.7V80.1c0-11.3-9.1-20.4-20.4-20.4-11.3 0-20.4 9.1-20.4 20.4v352.8l-85.7-85.7c-7.9-7.9-20.6-7.9-28.5 0-7.9 7.9-7.9 20.6 0 28.5l120.4 120.4c7.9 7.9 20.6 7.9 28.5 0l120.4-120.4c7.8-7.9 7.8-20.7-.1-28.5z"/></svg>
+            </button>
+          </div>
+          <div class="product-cell category">Amount<button class="sort-button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512"><path fill="currentColor" d="M496.1 138.3L375.7 17.9c-7.9-7.9-20.6-7.9-28.5 0L226.9 138.3c-7.9 7.9-7.9 20.6 0 28.5 7.9 7.9 20.6 7.9 28.5 0l85.7-85.7v352.8c0 11.3 9.1 20.4 20.4 20.4 11.3 0 20.4-9.1 20.4-20.4V81.1l85.7 85.7c7.9 7.9 20.6 7.9 28.5 0 7.9-7.8 7.9-20.6 0-28.5zM287.1 347.2c-7.9-7.9-20.6-7.9-28.5 0l-85.7 85.7V80.1c0-11.3-9.1-20.4-20.4-20.4-11.3 0-20.4 9.1-20.4 20.4v352.8l-85.7-85.7c-7.9-7.9-20.6-7.9-28.5 0-7.9 7.9-7.9 20.6 0 28.5l120.4 120.4c7.9 7.9 20.6 7.9 28.5 0l120.4-120.4c7.8-7.9 7.8-20.7-.1-28.5z"/></svg>
+          </button>
+        </div>
+        <div class="product-cell category">Date<button class="sort-button">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512"><path fill="currentColor" d="M496.1 138.3L375.7 17.9c-7.9-7.9-20.6-7.9-28.5 0L226.9 138.3c-7.9 7.9-7.9 20.6 0 28.5 7.9 7.9 20.6 7.9 28.5 0l85.7-85.7v352.8c0 11.3 9.1 20.4 20.4 20.4 11.3 0 20.4-9.1 20.4-20.4V81.1l85.7 85.7c7.9 7.9 20.6 7.9 28.5 0 7.9-7.8 7.9-20.6 0-28.5zM287.1 347.2c-7.9-7.9-20.6-7.9-28.5 0l-85.7 85.7V80.1c0-11.3-9.1-20.4-20.4-20.4-11.3 0-20.4 9.1-20.4 20.4v352.8l-85.7-85.7c-7.9-7.9-20.6-7.9-28.5 0-7.9 7.9-7.9 20.6 0 28.5l120.4 120.4c7.9 7.9 20.6 7.9 28.5 0l120.4-120.4c7.8-7.9 7.8-20.7-.1-28.5z"/></svg>
+        </button>
+      </div>
+      
+          <div class="product-cell status-cell">Add By<button class="sort-button">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512"><path fill="currentColor" d="M496.1 138.3L375.7 17.9c-7.9-7.9-20.6-7.9-28.5 0L226.9 138.3c-7.9 7.9-7.9 20.6 0 28.5 7.9 7.9 20.6 7.9 28.5 0l85.7-85.7v352.8c0 11.3 9.1 20.4 20.4 20.4 11.3 0 20.4-9.1 20.4-20.4V81.1l85.7 85.7c7.9 7.9 20.6 7.9 28.5 0 7.9-7.8 7.9-20.6 0-28.5zM287.1 347.2c-7.9-7.9-20.6-7.9-28.5 0l-85.7 85.7V80.1c0-11.3-9.1-20.4-20.4-20.4-11.3 0-20.4 9.1-20.4 20.4v352.8l-85.7-85.7c-7.9-7.9-20.6-7.9-28.5 0-7.9 7.9-7.9 20.6 0 28.5l120.4 120.4c7.9 7.9 20.6 7.9 28.5 0l120.4-120.4c7.8-7.9 7.8-20.7-.1-28.5z"/></svg>
+            </button>
+          </div>
+         
+        </div>
+
+<!----------------------------------------------------------------------------------------
+
+                                            PHP
+
+------------------------------------------------------------------------------------------>
+    <?php
+          
+          if(empty($from) OR empty($to)){
+            $sql = "SELECT * FROM tbl_expances;";
+            $resutl = $con->query($sql);
+          }
+          else{
+            
+          $sql = "SELECT * FROM tbl_expances WHERE ex_date >= '$from' AND ex_date <= '$to';";
+          $resutl = $con->query($sql);
+        }    
+            if(!$resutl){
+              die("Invalid Query: " . $con->error);
+            }  
+
+          //While loop to read database data
+          while($row = $resutl->fetch_assoc()){
+            echo " 
+            <div class= products-row>
+              <table id = 'myDataTable'> 
+              <div class=product-cell> <span>$row[Ex_ID]</span> </div>
+              <div class= product-cell category ><span class= cell-label >Category:</span>$row[Ex_Type]</div>
+              <div class= product-cell category ><span class= cell-label >Category:</span>$row[Ex_Name]</div>
+              <div class= product-cell sales ><span class= cell-label >Sales:</span>$row[Ex_amount]</div>
+              <div class= product-cell sales ><span class= cell-label >Sales:</span>$row[ex_date]</div>
+              <div class= product-cell sales ><span class= cell-label >Sales:</span>$row[User_ID]</div>
+
+              </table> 
+              
+            </div>
+
+            ";
+          }
+      ?>
+
+    </section>
+    </div>
+
+  
+    </div>
+</div>
+<!-- partial -->
+  <script  src="./script.js"></script>
+
+</body>
+</html>
